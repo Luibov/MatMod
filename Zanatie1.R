a7438e8d6dada6349ab092618ae616699cf9414c
Любовь <bucikaulitz@mail.ru>
2017-03-27 23:28:39 +0300

    Занятие 1

---
+++ Zanatie1.R
@@ -0,0 +1,5 @@
+zan1=list(mean(iris$Sepal.Length),mean(iris$Sepal.Width),mean(iris$Petal.Length), mean(iris$Petal.Width))
+zan1
+names(zan1)= c("Длина чашелистика","Ширина чашелистика", "Длина лепестков", "Ширина лепестков")
+names
+zan1
